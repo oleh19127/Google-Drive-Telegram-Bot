74d0ca654f3a225d5091e8f5143c7d75aab9b392
Oleh Husak <40184919+oleh312@users.noreply.github.com>
2023-04-19 17:12:48 +0300

    refactor some code

--- api/sleep/sleep.js
+++ api/sleep/sleep.js
@@ -1,7 +1,3 @@
-export const sleep = (ms) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, ms);
-  });
-};
+export function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
--- api/utils/get-file-names-from-folder.js
+++ api/utils/get-file-names-from-folder.js
@@ -1,6 +1,5 @@
 import { readdir } from "node:fs/promises";
 
 export const getFileNamesFromFolder = async (tmpFolder) => {
-  const filesInFolder = await readdir(tmpFolder);
-  return filesInFolder;
+  return await readdir(tmpFolder);
 };
